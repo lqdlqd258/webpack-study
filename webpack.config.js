c3868bae83827a77d916063605cdb089d8825d86
Liaojincheng <917710102@qq.com>
2019-05-30 03:57:28 +0800

    2019/5/30

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 //webpack 是采用node写法
-
+let path = require('path');
+// console.log(path.resolve(__dirname,'dist'));
+// console.log('dist');
 module.exports = {
+    mode:'development',//默认模式两种 production development，生产环境会压缩，开发环境不压缩，方便查看
     entry:'./src/index.js',//入口
-    output:{
-        filename:'index.js',
-
+    output:{ //出口
+        filename:'index.js',//打包后文件名
+        path:path.resolve(__dirname,'dist'),//路径有要求，必须是一个绝对路径,引入node的path模块，将相对路径解析成绝对路径
     }
 }
