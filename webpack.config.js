11d2b7286d301366f56418b39880c477ce089706
Liaojincheng <917710102@qq.com>
2019-07-14 23:17:17 +0800

    2019/7/14

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 let path = require('path');
 let htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    mode:'development',
     entry:{
         index:'./src/index.js'
+    },
+    module:{
+        noParse:/jquery/,//不去解析 jquery 中的依赖库
+        rules:[
+            {
+                test:/\.js$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+            }
+        ]
     },
     output:{
         filename:'index.js',
@@ -10,7 +25,9 @@ module.exports = {
     },
     plugins:[
         new htmlWebpackPlugin({
-            template:'./public/index.html'
+            template:'./public/index.html',
+            filename:'index.html',
+            chunks:['index']
         })
     ]
 }
